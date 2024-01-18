78e198d49648d5dc5de387434f817d08a4e6968c
Oliver Blaha <41994275+oliverblaha@users.noreply.github.com>
2024-01-18 17:14:13 +0100

    Add missing import

--- minimal/src/test/java/example/micronaut/MicronautguideTest.java
+++ minimal/src/test/java/example/micronaut/MicronautguideTest.java
@@ -3,6 +3,7 @@ package example.micronaut;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 @MicronautTest
 class MicronautguideTest {
